50bad1c4315f090dc03749ba144f9234b9d1fb67
Karthik Ragunath <mitkarthik@gmail.com>
2012-05-29 17:58:34 -0700

    removed DPID in msg format

--- Message.java
+++ Message.java
@@ -1,14 +1,11 @@
-
 public class Message{
   MSG_TYPE msg_type;
-  public int DPID;
   int PID;
   int AID;
   int Pnum;
   int Value;
-  public Message(MSG_TYPE mtype,int dpid,int pid,int aid,int pnum,int val){
+  public Message(MSG_TYPE mtype,int pid,int aid,int pnum,int val){
     msg_type=mtype;
-    DPID=dpid;
     PID=pid;
     AID=aid;
     Pnum=pnum;
@@ -17,14 +14,13 @@ public class Message{
   public void parseMessage(String msg){
     String[] split_msg = msg.split("\\.");
     this.msg_type=MSG_TYPE.valueOf(split_msg[0]);
-    this.DPID=Integer.parseInt(split_msg[1]);
-    this.PID=Integer.parseInt(split_msg[2]);
-    this.AID=Integer.parseInt(split_msg[3]);
-    this.Pnum=Integer.parseInt(split_msg[4]);
-    this.Value=Integer.parseInt(split_msg[5]);
+    this.PID=Integer.parseInt(split_msg[1]);
+    this.AID=Integer.parseInt(split_msg[2]);
+    this.Pnum=Integer.parseInt(split_msg[3]);
+    this.Value=Integer.parseInt(split_msg[4]);
   };
   public String createMessage(){
-    return this.msg_type+"."+this.DPID+"."+this.PID+"."+this.AID+"."+this.Pnum+"."+this.Value;
+    return this.msg_type+"."+this.PID+"."+this.AID+"."+this.Pnum+"."+this.Value;
   };
 }
 
--- Paxos.java
+++ Paxos.java
@@ -1,13 +1,21 @@
 import java.lang.Thread;
+import java.util.Random;
+
 enum MSG_TYPE {PREPARE, PROMISE, ACCEPT, ACCEPTED};
+enum STATE {PREPARE,PROMISE_ACK,PROMISE_NACK,ACCEPT,ACCEPTED};
+
 
 public class Paxos implements Runnable{
   Network network;
-
+  int MAX_PROPNUM=200;
+  int[] DisjointProposalNum=new int[MAX_PROPNUM];
   public Paxos(Network network) {
     this.network=network;
-  }
-
+    Random rand = new Random(20120528);
+    for(int i=0;i<MAX_PROPNUM;i++){
+      DisjointProposalNum[i]=i;
+    }
+}
   /** This should start your Paxos implementation and return immediately. */
   public void runPaxos() {
 //
@@ -17,33 +25,75 @@ public class Paxos implements Runnable{
       t.start();
     }
   }
+  
+  public int nextProposalNumber(int PID,int cur_Pnum){
+    int i=0;
+    while(true){
+      if(DisjointProposalNum[i]<cur_Pnum)
+	i++;
+      else{
+        if(DisjointProposalNum[i]%network.numProposers()==PID)
+          return DisjointProposalNum[i];
+        else
+          i++;
+      }
+      if(i==MAX_PROPNUM)
+        return -1;
+    }
+  }
   public void run(){
     try{
-      while(true){
 	Thread t= Thread.currentThread();
 	Channel c=network.getChannel(Integer.parseInt(t.getName()));
 	if(c.index<network.numProposers){
 		//Proposer code goes here
-	        Message m=new Message(MSG_TYPE.PROMISE,0,c.index,2,12,-1);
-		c.sendMessage(2,m.createMessage());
-
+		//Proposer Initiated 
+		//Send Prepare 
+		STATE state=STATE.PROMISE_ACK;
+                int cur_Pnum=-1;
+		while(true){
+			if(c.isDistinguished()){ 
+//I am distinguished proposer, if not I shall be idle and querying constantly if I become one.
+				while(true){
+					switch(state){
+						case PREPARE:
+							Message m=new Message(MSG_TYPE.PROMISE,c.index,2,nextProposalNumber(c.index,cur_Pnum),-1);
+							for(int a=network.numProposers();a<(network.numProposers+network.numAcceptors);a++)
+								c.sendMessage(a,m.createMessage());
+							break;
+						case PROMISE_ACK:
+							System.out.println("I have nt recvd ACK yet");
+							break;
+					}
+				Thread.sleep(1000);
+			}
+			}
+			else{
+				System.out.println("I am not Distinguished Proposer, so sitting idle");
+				Thread.sleep(1000);
+			}
+	
+			
+		}
 	}
 	if(c.index>=network.numProposers && c.index<(network.numProposers+network.numAcceptors)){
 		//Acceptor code goes here
-	        Message m=new Message(MSG_TYPE.PROMISE,0,0,0,0,0);
-		m.parseMessage(c.receiveMessage());
-		System.out.println(m.createMessage());
-
-		System.out.println(m.DPID+""+m.AID+""+m.PID);
+		while(true){
+	        Message m=new Message(MSG_TYPE.PROMISE,0,0,0,0);
+		String msg=c.receiveMessage());
+		if(msg!=null){
+			msg.parseMessage()
+		System.out.println("Acceptor- "+c.index+"received: "+m.createMessage());
+//		System.out.println(m.DPID+""+m.AID+""+m.PID);
+		Thread.sleep(1000);
+		}
 
 	}
 	if(c.index>=(network.numProposers+network.numAcceptors)){
 		//Learner code goes here
 	        System.out.println(c.index+"I am learner");
 	}
-  
-        Thread.sleep(1000);
-      }
+
     }
     catch(Exception e){e.printStackTrace();}
   }
