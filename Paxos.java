ddec29fd219d4596ac66a6cb365a3c0b69b3a7d1
Karthik Ragunath <mitkarthik@gmail.com>
2012-05-29 19:00:33 -0700

    pushing proposer code

--- Paxos.java
+++ Paxos.java
@@ -1,13 +1,11 @@
 import java.lang.Thread;
 import java.util.Random;
 
-enum MSG_TYPE {PREPARE, PROMISE, ACCEPT, ACCEPTED};
-enum STATE {PREPARE,PROMISE_ACK,PROMISE_NACK,ACCEPT,ACCEPTED};
-
+enum MSG_TYPE {PREPARE, PROMISE, ACCEPT, ACCEPTED,LEARN,NACK_PROMISE,NACK_ACCEPTED};
 
 public class Paxos implements Runnable{
   Network network;
-  int MAX_PROPNUM=200;
+  int MAX_PROPNUM=20000;
   int[] DisjointProposalNum=new int[MAX_PROPNUM];
   public Paxos(Network network) {
     this.network=network;
@@ -43,57 +41,86 @@ public class Paxos implements Runnable{
   }
   public void run(){
     try{
-	Thread t= Thread.currentThread();
-	Channel c=network.getChannel(Integer.parseInt(t.getName()));
-	if(c.index<network.numProposers){
-		//Proposer code goes here
-		//Proposer Initiated 
-		//Send Prepare 
-		STATE state=STATE.PROMISE_ACK;
-                int cur_Pnum=-1;
-		while(true){
-			if(c.isDistinguished()){ 
-//I am distinguished proposer, if not I shall be idle and querying constantly if I become one.
-				while(true){
-					switch(state){
-						case PREPARE:
-							Message m=new Message(MSG_TYPE.PROMISE,c.index,2,nextProposalNumber(c.index,cur_Pnum),-1);
-							for(int a=network.numProposers();a<(network.numProposers+network.numAcceptors);a++)
-								c.sendMessage(a,m.createMessage());
-							break;
-						case PROMISE_ACK:
-							System.out.println("I have nt recvd ACK yet");
-							break;
-					}
-				Thread.sleep(1000);
-			}
-			}
-			else{
-				System.out.println("I am not Distinguished Proposer, so sitting idle");
-				Thread.sleep(1000);
-			}
-	
-			
-		}
+      Thread t= Thread.currentThread();
+      Channel c=network.getChannel(Integer.parseInt(t.getName()));
+      if(c.index<network.numProposers){
+	//Proposer code goes here
+	//Proposer Initiated 
+	//Send Prepare 
+	STATE state=STATE.PROMISE_ACK;
+        int cur_Pnum=-1;
+	while(true){
+  	  if(c.isDistinguished()){ 
+	  //I am distinguished proposer, if not I shall be idle and querying constantly if I become one.
+	    Message m_prepare=new Message(MSG_TYPE.PREPARE,c.index,2,nextProposalNumber(c.index,cur_Pnum),-1);
+	    for(int a=network.numProposers();a<(network.numProposers+network.numAcceptors);a++)
+  	      c.sendMessage(a,m_prepare.createMessage());
+	    while(true){
+	      String msg=c.receiveMessage(); // infintely query Message buffer for new message;
+//              if(timeout)
+// 		should resend prepare
+              if(msg!=null){
+	        Message m=new Message(MSG_TYPE.PREPARE,0,0,0,0); //create a dummy Message object
+   	        m.parseMessage(msg); //parse the received message to the Message object
+                switch(m.msg_type){
+                  case PROMISE: //if the proposer received promise, send a accept.
+                    int value=-1;
+                    if(m.Value==-1)
+		      value=c.getInitialValue();
+		    else
+                      value=m.Value;
+		    Message m_accept=new Message(MSG_TYPE.ACCEPT,c.index,m.AID,m.Pnum,value);   //what if hacker changes c.index?        
+                    c.sendMessage(m.AID,m_accept.createMessage());
+                    break;
+                  case NACK_PROMISE: //proposal rejected coz Accepter has promised higher proposal
+                    int next_Pnum=nextProposalNumber(c.index,m.Pnum);
+		    if(next_Pnum==-1)
+                       System.out.println("Reached MAX_PROPNUM, bail out by throwing exception");
+ 		    Message m_newprepare=new Message(MSG_TYPE.PREPARE,c.index,m.AID,next_Pnum,-1);  //what if hacker changes c.index?        
+                    c.sendMessage(m.AID,m_newprepare.createMessage());
+                    break;
+                  case ACCEPTED: //value has been accepted. Keep quiet.
+              	    break;
+                  case NACK_ACCEPTED: 
+		    next_Pnum=nextProposalNumber(c.index,m.Pnum);
+		    if(next_Pnum==-1)
+                       System.out.println("Reached MAX_PROPNUM, bail out by throwing exception");
+ 		    m_newprepare=new Message(MSG_TYPE.PREPARE,c.index,m.AID,next_Pnum,m.Value);  //what if hacker changes c.index?        
+                    c.sendMessage(m.AID,m_newprepare.createMessage());
+                    break;
+                }
+              }
+              else{ 
+	        Thread.sleep(1000); //Message queue empty so sleeping.
+              }  
+	    }
+	  }
+	  else{
+	    System.out.println("I am not Distinguished Proposer, so sitting idle");
+	    Thread.sleep(1000);
+	  }
 	}
-	if(c.index>=network.numProposers && c.index<(network.numProposers+network.numAcceptors)){
-		//Acceptor code goes here
-		while(true){
-	        Message m=new Message(MSG_TYPE.PROMISE,0,0,0,0);
-		String msg=c.receiveMessage());
-		if(msg!=null){
-			msg.parseMessage()
-		System.out.println("Acceptor- "+c.index+"received: "+m.createMessage());
-//		System.out.println(m.DPID+""+m.AID+""+m.PID);
-		Thread.sleep(1000);
-		}
-
+      }
+      // Code for Acceptor Behaviour
+      if(c.index>=network.numProposers && c.index<(network.numProposers+network.numAcceptors)){
+	//Acceptor code goes here
+	while(true){
+          Message m=new Message(MSG_TYPE.PROMISE,0,0,0,0);
+          String msg=c.receiveMessage();
+	  if(msg!=null){
+	    m.parseMessage(msg);
+	    System.out.println("Acceptor- "+c.index+"received: "+m.createMessage());
+	  }
+	  else{
+	    System.out.println("NULL message received");
+	  }
+	  Thread.sleep(1000);
 	}
-	if(c.index>=(network.numProposers+network.numAcceptors)){
-		//Learner code goes here
-	        System.out.println(c.index+"I am learner");
-	}
-
+      }
+      if(c.index>=(network.numProposers+network.numAcceptors)){
+	//Learner code goes here
+	System.out.println(c.index+"I am learner");
+      }
     }
     catch(Exception e){e.printStackTrace();}
   }
